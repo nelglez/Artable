748611b34ebe5c880d09d17411d981594f5ca9a0
Nelson Gonzalez <nelglezfl@gmail.com>
2019-08-09 16:32:36 -0400

    Upload image to firebase storage.

--- ArtableAdmin/Controllers/AddEditCategoryViewController.swift
+++ ArtableAdmin/Controllers/AddEditCategoryViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseStorage
 
 class AddEditCategoryViewController: UIViewController {
     @IBOutlet weak var nameTextField: UITextField!
@@ -29,6 +30,50 @@ class AddEditCategoryViewController: UIViewController {
 
     @IBAction func addCategoryButtonPressed(_ sender: UIButton) {
         
+        activityIndicator.startAnimating()
+        uploadImageThenDocument()
+    }
+    
+    private func uploadImageThenDocument() {
+        guard let image = categoryImage.image, let categoryName = nameTextField.text, categoryName.isNotEmpty else {
+            simpleAlert(title: "Error", message: "Must have category image and name.")
+            return
+        }
+        //Turn image into data
+        guard let imageData = image.jpegData(compressionQuality: 0.2) else { return }//1. full resolution. 0. compressed
+        
+        //create a storage image ref
+        let imageRef = Storage.storage().reference().child("/categoryImages/\(categoryName).jpg")
+        //set meta data
+        let metaData = StorageMetadata()
+        metaData.contentType = "image/jpg"
+        
+        //upload data
+        imageRef.putData(imageData, metadata: metaData) { (metaData, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                self.simpleAlert(title: "Error", message: "Unable to upload image.")
+                self.activityIndicator.stopAnimating()
+                return
+            }
+            //retrieve the download url.
+            imageRef.downloadURL(completion: { (url, error) in
+                if let error = error {
+                    print(error.localizedDescription)
+                    self.simpleAlert(title: "Error", message: "Unable to upload image.")
+                    self.activityIndicator.stopAnimating()
+                    return
+                }
+                guard let url = url else { return }
+                
+                print("URL: \(url)")
+                
+                //upload new category document to the firestore category collection
+            })
+        }
+    }
+    
+    private func uploadDocument() {
         
     }
     
